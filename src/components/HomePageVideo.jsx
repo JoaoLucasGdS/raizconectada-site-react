57200bb22bcb752a2b9085c2f38deae0840af074
Joao Lucas Gomes de Souza <joaolucasgdesouza@gmail.com>
2025-09-05 19:10:35 -0300

    Delete scrool blocking from HomePageVideo component

--- src/components/HomePageVideo.jsx
+++ src/components/HomePageVideo.jsx
@@ -1,16 +1,7 @@
 import './css/HomePageVideo.css';
 import homePageVideoFile from '../assets/HomePageVideo.mp4'
-import { useEffect } from "react";
 
 function HomePageVideo(){
-
-    useEffect(() => {
-        // add class to disable body scroll while component is mounted
-        document.body.classList.add('no-scroll');
-        return () => {
-        document.body.classList.remove('no-scroll');
-        };
-    }, []);
 
     return (
         <div className='DivHomePageVideo'>
