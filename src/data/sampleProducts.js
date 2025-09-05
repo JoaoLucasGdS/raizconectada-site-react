04dce572556b454f11362cd3ef744164d5e9b85f
Joao Lucas Gomes de Souza <joaolucasgdesouza@gmail.com>
2025-09-05 14:50:57 -0300

    Add data for review page

---
+++ src/data/sampleProducts.js
@@ -0,0 +1,14 @@
+const sampleProducts = [
+    { id: 1, image: "src/assets/products/p1.jpg", alt: "Banana" },
+    { id: 2, image: "src/assets/products/p2.jpg", alt: "Maça" },
+    { id: 3, image: "src/assets/products/p3.jpg", alt: "Melancia" },
+    { id: 4, image: "src/assets/products/p4.jpg", alt: "Laranja" },
+    { id: 5, image: "src/assets/products/p5.jpg", alt: "Mamão" },
+    { id: 6, image: "src/assets/products/p6.jpg", alt: "Peira" },
+    { id: 7, image: "src/assets/products/p7.jpg", alt: "Kiwi" },
+    { id: 8, image: "src/assets/products/p8.jpg", alt: "Romã" },
+    { id: 9, image: "src/assets/products/p9.jpg", alt: "Acerola" },
+    { id: 10, image: "src/assets/products/p10.jpg", alt: "Uva" },
+];
+
+export default sampleProducts;
