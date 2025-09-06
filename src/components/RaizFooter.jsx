f424f94fb2ba60b95418ba67396ed25be0b65b11
Joao Lucas Gomes de Souza <joaolucasgdesouza@gmail.com>
2025-09-06 16:58:31 -0300

    Complete component RaizFooter translation

--- src/components/RaizFooter.jsx
+++ src/components/RaizFooter.jsx
@@ -1,14 +1,18 @@
+import { useTranslation } from "react-i18next";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faInstagram, faSquareFacebook, faWhatsapp } from '@fortawesome/free-brands-svg-icons';
 
 import "./css/RaizFooter.css"
 
 function RaizFooter(){
+
+    const { t } = useTranslation();
+
     return (
             <footer className="mt-auto d-flex flex-column">
                 <ul class="ul-footer">
                     
-                    <li><a href="#">Quem somos</a></li>
+                    <li><a href="#">{t("footer.aboutus")}</a></li>
                     <li><a href="#"><FontAwesomeIcon icon={faInstagram} /></a></li>
                     <li><a href="#"><FontAwesomeIcon icon={faSquareFacebook} /></a></li>
                     <li><a href="#"><FontAwesomeIcon icon={faWhatsapp} /></a></li>
