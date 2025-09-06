4c37906ac22ee1a252132eae9d7de59b294506f2
Joao Lucas Gomes de Souza <joaolucasgdesouza@gmail.com>
2025-09-06 01:38:03 -0300

    Modify root layout for stylistic reason

--- src/components/RootLayout.jsx
+++ src/components/RootLayout.jsx
@@ -1,4 +1,5 @@
 import { Outlet, useLocation } from "react-router-dom";
+import { useEffect } from "react";
 
 import RaizHeader from "./RaizHeader";
 
@@ -7,6 +8,20 @@ function RootLayout() {
     const location = useLocation();
 
     const isHomePage = location.pathname === "/";
+
+    //setting up body.home so we can make the background color on homepage green using the homepage css
+    //Doing this since without it the HomePage Form will look like shit having a white background and all
+    useEffect(() => {
+        if (isHomePage) {
+            document.body.classList.add('home');
+        } else {
+            document.body.classList.remove('home');
+        }
+
+        return () => {
+            document.body.classList.remove('home');
+        };
+    }, [isHomePage]);
 
     return (
         <>
