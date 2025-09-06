a4846f844058c091ce84f765ea22f30cf58cb349
Joao Lucas Gomes de Souza <joaolucasgdesouza@gmail.com>
2025-09-06 16:42:23 -0300

    Add i18n.js

---
+++ src/i18n.js
@@ -0,0 +1,20 @@
+import i18n from "i18next";
+import { initReactI18next } from "react-i18next";
+
+import en from "./locales/en/translation.json";
+import ptBR from "./locales/pt-BR/translation.json";
+
+const resources = {
+  en: { translation: en },
+  "pt-BR": { translation: ptBR },
+};
+
+i18n.use(initReactI18next).init({
+  resources,
+  fallbackLng: "pt-BR",
+  supportedLngs: ["en", "pt-BR"],
+  interpolation: { escapeValue: false },
+  react: { useSuspense: false },
+});
+
+export default i18n;
