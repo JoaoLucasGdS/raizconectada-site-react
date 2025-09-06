f5a6d31e3380c76c645bca16f5c4fcb88d775dfa
Joao Lucas Gomes de Souza <joaolucasgdesouza@gmail.com>
2025-09-06 15:34:13 -0300

    Add mock data for visualization

---
+++ src/data/mockOrders.js
@@ -0,0 +1,25 @@
+const mockOrders = [
+  {
+    id: 1234,
+    items: ["Banana x10"],
+    date: "12 de abril de 2024",
+    status: "delivered",
+    image: "src/assets/products/p1.jpg",
+  },
+  {
+    id: 1233,
+    items: ["Laranja x6"],
+    date: "10 de abril de 2024",
+    status: "canceled",
+    image: "src/assets/products/p4.jpg",
+  },
+  {
+    id: 1222,
+    items: ["Melancia x2"],
+    date: "7 de setembro de 2024",
+    status: "pending",
+    image: "src/assets/products/p3.jpg",
+  },
+];
+
+export default mockOrders;
---
+++ src/data/mockProfiles.js
@@ -0,0 +1,39 @@
+const users = [
+        {
+            name: "Maria Silva",
+            email: "maria@example.com",
+            number: "11 91234-5678",
+            imgUrl: "https://i.pravatar.cc/56?u=1",
+            isVerified: false,
+        },
+        {
+            name: "João Lucas",
+            email: "joao@example.com",
+            number: "21 99876-5432",
+            imgUrl: "https://i.pravatar.cc/56?u=2",
+            isVerified: true,
+        },
+        {
+            name: "Ana Paula",
+            email: "ana@example.com",
+            number: "31 98765-4321",
+            imgUrl: "https://i.pravatar.cc/56?u=3",
+            isVerified: false,
+        },
+        {
+            name: "Carlos Eduardo",
+            email: "carlos@example.com",
+            number: "41 97654-3210",
+            imgUrl: "https://i.pravatar.cc/56?u=4",
+            isVerified: true,
+        },
+        {
+            name: "Fernando Souza",
+            email: "fernando@example.com",
+            number: "51 96543-2109",
+            imgUrl: "https://i.pravatar.cc/56?u=5",
+            isVerified: false,
+        },
+    ];
+
+export default users;
