66726aea99d21da2f070ed0a8376ed715b1c5077
Joao Lucas Gomes de Souza <joaolucasgdesouza@gmail.com>
2025-09-06 17:23:23 -0300

    Update order items names to english

--- src/data/mockOrders.js
+++ src/data/mockOrders.js
@@ -1,21 +1,21 @@
 const mockOrders = [
   {
     id: 1234,
-    items: ["Banana x10"],
+    items: ["banana"],
     date: "12 de abril de 2024",
     status: "delivered",
     image: "src/assets/products/p1.jpg",
   },
   {
     id: 1233,
-    items: ["Laranja x6"],
+    items: ["orange"],
     date: "10 de abril de 2024",
     status: "canceled",
     image: "src/assets/products/p4.jpg",
   },
   {
     id: 1222,
-    items: ["Melancia x2"],
+    items: ["watermelon"],
     date: "7 de setembro de 2024",
     status: "pending",
     image: "src/assets/products/p3.jpg",
