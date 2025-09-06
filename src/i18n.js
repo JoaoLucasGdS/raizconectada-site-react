e49d18959956bc43ec4e0301844b256e717afc75
Joao Lucas Gomes de Souza <joaolucasgdesouza@gmail.com>
2025-09-06 17:51:03 -0300

    Update

--- src/i18n.js
+++ src/i18n.js
@@ -3,16 +3,20 @@ import { initReactI18next } from "react-i18next";
 
 import en from "./locales/en/translation.json";
 import ptBR from "./locales/pt-BR/translation.json";
+import es from "./locales/es/translation.json";
+import ja from "./locales/ja/translation.json";
 
 const resources = {
   en: { translation: en },
   "pt-BR": { translation: ptBR },
+  es: { translation: es },
+  ja: { translation: ja },
 };
 
 i18n.use(initReactI18next).init({
   resources,
   fallbackLng: "pt-BR",
-  supportedLngs: ['pt-BR', 'en', 'es', 'ja'],
+  supportedLngs: ["pt-BR", "en", "es", "ja"],
   interpolation: { escapeValue: false },
   react: { useSuspense: false },
 });
