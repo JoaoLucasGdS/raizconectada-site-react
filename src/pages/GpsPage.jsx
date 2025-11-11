f711cded5eaa67602ff869855555bf4d11743b07
Joao Lucas Gomes de Souza <joaolucasgdesouza@gmail.com>
2025-11-11 20:02:19 -0300

    Change MapLeaflet component parameters location to its parent component GpsPage

--- src/pages/GpsPage.jsx
+++ src/pages/GpsPage.jsx
@@ -2,10 +2,11 @@ import MapLeaflet from "../components/MapLeaflet";
 
 function GpsPage() {
   return (
-    <>
-    
-    <MapLeaflet />
-    </>
+    <MapLeaflet 
+      center={[-23.55052, -46.633308]} 
+      zoom={13} 
+      markerText="Você está aqui" 
+    />
   );
 }
 
